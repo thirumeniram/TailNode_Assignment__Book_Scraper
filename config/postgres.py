5949878ba000c6720cbe94b37fb5b87dcf8f1ea6
thirumeniram Arumugam <thirumeniram25@gmail.com>
2024-03-15 12:47:19 +0530

    fourth commit

--- config/postgres.py
+++ config/postgres.py
@@ -1,4 +1,4 @@
-# I am using ElephantSQL  for PostgreSQL hosting. 
-# This is the database connection string for PostgreSQL(ElephantSQL ).
+#I am using ElephantSQL  for PostgreSQL hosting. 
+#This is the database connection string for PostgreSQL(ElephantSQL ).
 
 DATABASE_URL="postgres://dhvhovka:AQODQxgRqXiKhnxDg__ZMEqkAETj0eI3@cornelius.db.elephantsql.com/dhvhovka"
