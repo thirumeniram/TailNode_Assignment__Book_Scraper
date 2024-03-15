98fad7055ac2901956b623778b6a9fbe4b5d7400
thirumeniram Arumugam <thirumeniram25@gmail.com>
2024-03-15 12:49:15 +0530

    fourth commit

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from model.book import initialize_db
 from controller.scrape_controller import scrape_and_store_books
 
 def main():
-    initialize_db()  # Initializing the database and create table if not exists
-    scrape_and_store_books()  # Scraping and storing books data
+    initialize_db()  #Initializing the database and create table if not exists
+    scrape_and_store_books()  #Scraping and storing books data
 
 if __name__=='__main__':
     main()
